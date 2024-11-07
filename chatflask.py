18e25bead4c12d292cdf128e2f42e4a532a9e555
Nehaja13 <130177184+Nehaja13@users.noreply.github.com>
2024-11-07 23:37:46 +0530

    Update chatflask.py

--- chatflask.py
+++ chatflask.py
@@ -72,8 +72,10 @@ def health():
     return jsonify({'status': 'API is up and running'}), 200
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    # app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
+    print("Starting Flask app...")
+
     
 
 
