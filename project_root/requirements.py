c6cf77232b2991397e11404d562caddc15320f68
Nehaja13 <130177184+Nehaja13@users.noreply.github.com>
2024-11-08 22:33:10 +0530

    Delete project_root/requirements.py

--- project_root/requirements.py
+++
@@ -1,9 +0,0 @@
-Flask==2.3.2
-requests==2.31.0
-Pillow==11.0.0
-numpy==1.25.0
-torch==2.0.0
-torchvision==0.15.1  # Version compatible with torch 2.0.0
-sympy==1.13.3
-networkx==3.4.2
-cmake==3.30.5  # Optional, if required by any dependency
